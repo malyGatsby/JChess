616e6eb454537445cb4647834060bf2c9e6b4167
malyGatsby <mikolaj.krymarys@gmail.com>
2021-05-03 21:01:08 +0200

    immutable tile class

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -37,8 +37,12 @@ public abstract class Tile {
         return ImmutableMap.copyOf(emptyTileMap);
     }
 
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
     //constructor for any tile
-    public Tile(int tileCoordinate) {
+    private Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
