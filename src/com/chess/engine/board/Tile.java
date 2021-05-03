a05f5d3a5230ef9a8ef17267027aba28ba1f2e9e
malyGatsby <mikolaj.krymarys@gmail.com>
2021-05-03 20:52:14 +0200

    immutable tile class

---
+++ src/com/chess/engine/board/Tile.java
@@ -0,0 +1,89 @@
+package com.chess.engine.board;
+
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by malyGatsby on 03.05.2021
+ */
+
+// class that represents the Tile; is abstract because
+// there are only 2 possible tiles: empty one and one
+// with piece on it
+public abstract class Tile {
+
+    //further fields are final so they are immutable
+    // therefore thread safe
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    // this method will be used to check possible empty
+    // tiles from map instead of creating them by hand
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        //ImmutableMap is from guava library - provides
+        // map that noone will be able to modify this map
+        // after its created
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    //constructor for any tile
+    public Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    //class representing emptytile - only describing
+    //tile coordinate
+    public static final class EmptyTile extends Tile {
+
+        EmptyTile(final int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    //class representing emptytile - describing tile
+    //coordinate and piece sitting on it
+    public static final class OccupiedTile extends Tile {
+
+        private final Piece pieceOnTile;
+
+        OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
---
+++ src/com/chess/engine/pieces/Piece.java
@@ -0,0 +1,12 @@
+package com.chess.engine.pieces;
+
+/**
+ * Created by malyGatsby on 03.05.2021
+ */
+
+
+public class Piece {
+
+
+
+}
